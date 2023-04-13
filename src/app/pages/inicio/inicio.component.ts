8fb7e1c9c22b639696ba666a3635bf7cc62fa54d
cindy <cindy_calderon_rios@hotmail.com>
2023-04-13 18:20:37 -0500

    tercer cambio

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 export class InicioComponent {
 nombre = 'Cindy';
 apellido = 'Calderon';
+loadingVisible=false;
+
+visualizarLoading(){
+  this.loadingVisible = true;
+  //Codigo de loading de 2 segundos.
+  setTimeout(() => {
+    this.loadingVisible = false;
+  }, 2000);
+}
+
 }
