6abf169120fe5ee8d48bd628f951ba0580afe0ae
cindy <cindy_calderon_rios@hotmail.com>
2023-04-17 21:00:51 -0500

    cuarto cambio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { InicioComponent } from "./pages/inicio/inicio.component";
+import { PaginaTablaComponent } from "./pages/pagina-tabla/pagina-tabla.component";
 
 const routes: Routes = [
-    { path: 'inicio', component: InicioComponent }
+    { path: '', redirectTo: 'inicio', pathMatch: 'full'},  //direcciona directamente al inicio, automaticamente.
+    { path: 'inicio', component: InicioComponent },        //Declara la ruta inicio.
+    { path: 'tabla', component: PaginaTablaComponent }     //Declara la ruta de la pagina tabla.
 ];
 
 @NgModule (
--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-inicio',
@@ -10,12 +12,21 @@ nombre = 'Cindy';
 apellido = 'Calderon';
 loadingVisible=false;
 
+constructor(private ruta: Router){
+
+}
+
+
 visualizarLoading(){
   this.loadingVisible = true;
   //Codigo de loading de 2 segundos.
   setTimeout(() => {
     this.loadingVisible = false;
   }, 2000);
+}
+
+irPaginaTabla(){
+  this.ruta.navigate(['tabla']);
 }
 
 }
