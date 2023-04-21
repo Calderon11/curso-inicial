bc8bf7ed3ef90613004a48d53d6ea7863147d095
cindy <cindy_calderon_rios@hotmail.com>
2023-04-21 18:40:24 -0500

    noveno cambio

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
-
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-inicio',
@@ -13,10 +13,11 @@ export class InicioComponent implements OnInit {
 nombre = 'Cindy';
 apellido = 'Calderon';
 loadingVisible=false;
-formUsuario!: FormGroup;  //Es del formulario.
+formEmpleado!: FormGroup;  //Es del formulario.
 
 constructor(private ruta: Router,
-            private mensajes: MessageService){
+            private mensajes: MessageService,
+            private serviceEmpleado: EmpleadoService ){
 }
 
 ngOnInit(): void {
@@ -39,22 +40,41 @@ visualizarLoading(){
 }
 
 buildFormulario(){
-  this.formUsuario = new FormGroup(
+  this.formEmpleado = new FormGroup(
     {
-      nombre: new FormControl('',[Validators.required]),
-      apellido: new FormControl('',[Validators.required])
+      name: new FormControl('',[Validators.required]),
+      salary: new FormControl('',[Validators.required]),
+      age: new FormControl('',[Validators.required])
     }
   );
 }
 
 
 verficar(){
-  console.log(this.formUsuario);
-  if (this.formUsuario.valid){
+  console.log(this.formEmpleado);
+  if (this.formEmpleado.valid){
     this.mensajes.add({ severity: 'success', summary: 'Formulario Correcto', detail: 'Exito' });
   } else {
     this.mensajes.add({ severity: 'error', summary: 'Error', detail: 'Por favor llene los campos requeridos' });
   }
 }
 
+
+
+crearEmpleado(){
+  this.serviceEmpleado.
+    createEmployee( this.formEmpleado.value.name, 
+                    this.formEmpleado.value.salary, 
+                    this.formEmpleado.value.age).subscribe({
+                      next: (resp)=>{
+                        console.log(resp);
+                        this.formEmpleado.reset();
+                        this.mensajes.add({ severity: 'success', summary: 'Empleado creado con éxito', detail: 'Exito' });
+                      },
+                      error: (resp) => {
+                        console.log(resp);
+                        this.mensajes.add({ severity: 'error', summary: 'Error', detail: 'Hubo un error' });
+                      }
+                    });
+}
 }
